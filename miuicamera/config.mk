26a158e1d93b79da03742ea0334a86aebdb0c78a
StarkDroid <gamerprince.exp@gmail.com>
2021-05-15 16:10:21 +0200

    Fix path
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- miuicamera/config.mk
+++ miuicamera/config.mk
@@ -1,11 +1,11 @@
 PRODUCT_SOONG_NAMESPACES += \
-    vendor/xiaomi/miuicamera
+    vendor/xiaomi/phoenix/miuicamera
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_VENDOR)/camera)
+    $(call find-copy-subdir-files,*,vendor/xiaomi/phoenix/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/phoenix/miuicamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/phoenix//miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/phoenix/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_VENDOR)/camera)
 
 PRODUCT_PACKAGES += \
     MiuiCamera \
